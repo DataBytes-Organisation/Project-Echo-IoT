22191e6846764a20736e8d7c7f91df584929f5c8
Adam <amedwards@deakin.edu.au>
2024-09-16 15:05:42 +1000

    updates

--- ingestion_backend/python/ingestion/src/mqtt_client.py
+++ ingestion_backend/python/ingestion/src/mqtt_client.py
@@ -17,23 +17,19 @@ CLIENT_ID = "python_ingestion_backend1"
 CLIENT_TRANSPORT = "websockets"
 SESSION_EXPIRY_INTERVAL = 30 * 60
 
-class MQTTClientBuilder(BuilderBase):
+
+class OnConnectCallbackBuilder(BuilderBase):
     """
-    Concrete implementation of builder.
-    This builder builds an instance of paho.mqtt.client.Client connected to the
-    IOT ingestion backend.
+    Builds callback callable used as on_connect callback on Paho MQTT client.
     """
-    def build(self, **kwargs) -> None:
+    def build(self, topic: str) -> None:
         """
-        Builds paho.mqtt.client.Client and sets as result.
+        Builds callback callable used as on_connect callback on Paho MQTT client.
 
         Arguments:
             self
-            **kwargs
-                user_name: str
-                password: str
-                topic: str
-                endpoint: str
+            topic: str
+                MQTT topic on broker to subscribe to
         Returns:
             None
         """
@@ -64,14 +60,33 @@ class MQTTClientBuilder(BuilderBase):
             else:
                 # we should always subscribe from on_connect callback to be sure
                 # our subscribed is persisted across reconnections.
-                client.subscribe(kwargs["topic"])
+                client.subscribe(topic)
                 print("Connected to broker")
+        # pylint: enable=unused-argument
+        self.result = on_connect
 
+
+class OnMessageCallbackBuilder(BuilderBase):
+    """
+    Builds callback callable used as on_message callback on Paho MQTT client.
+    """
+    def build(self) -> None:
+        """
+        Builds callback callable used as on_message callback on Paho MQTT client.
+
+        Arguments:
+            self
+            topic: str
+                MQTT topic on broker to subscribe to
+        Returns:
+            None
+        """
+        # pylint: disable=unused-argument
         def on_message(
                 client: mqtt.Client,
                 userdata: Any,
                 msg: mqtt.MQTTMessage
-            ) -> None:
+        ) -> None:
             """
             Callback set as on_message property on Paho MQTT client.
             See https://eclipse.dev/paho/files/paho.mqtt.python/html/client.html#paho.mqtt.client
@@ -85,26 +100,56 @@ class MQTTClientBuilder(BuilderBase):
                 None
             """
             MessageProcessor().process(msg)
-        # pylint: enable=unused-argument
 
+
+        # pylint: enable=unused-argument
+        self.result = on_message
+
+
+class MQTTClientBuilder(BuilderBase):
+    """
+    Concrete implementation of builder.
+    This builder builds an instance of paho.mqtt.client.Client connected to the
+    IOT ingestion backend.
+    """
+    def build(
+            self,
+            user_name: str,
+            password: str,
+            topic: str,
+            endpoint: str
+    ) -> None:
+        """
+        Builds paho.mqtt.client.Client and sets as result.
+
+        Arguments:
+            self
+            user_name: str
+            password: str
+            topic: str
+            endpoint: str
+        Returns:
+            None
+        """
         client: mqtt.Client = mqtt.Client(
             client_id=CLIENT_ID,
             callback_api_version=mqtt.CallbackAPIVersion.VERSION2,
             transport=CLIENT_TRANSPORT,
             protocol=mqtt.MQTTv5
         )
-        client.on_connect = on_connect
-        client.on_message = on_message
+
+        client.on_connect = OnConnectCallbackBuilder().get(topic=topic)
+        client.on_message = OnMessageCallbackBuilder().get()
         client.tls_set()
         client.username_pw_set(
-            username=kwargs["user_name"],
-            password=kwargs["password"]
+            username=user_name,
+            password=password
         )
 
         properties: Properties = Properties(PacketTypes.CONNECT)
         properties.SessionExpiryInterval = SESSION_EXPIRY_INTERVAL # in seconds
 
-        client.connect(kwargs["endpoint"],
+        client.connect(endpoint,
             port=443,
             clean_start=mqtt.MQTT_CLEAN_START_FIRST_ONLY,
             properties=properties,
